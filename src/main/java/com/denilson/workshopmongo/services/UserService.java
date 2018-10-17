d53128959cf6e284d79ed2e30d726cb7afe91b0c
Denilson Leite <denilson.leite.ext@dasa.com.br>
2018-10-17 20:54:25 -0300

    Tratamento de erro.

--- src/main/java/com/denilson/workshopmongo/services/UserService.java
+++ src/main/java/com/denilson/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.denilson.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.denilson.workshopmongo.domain.User;
 import com.denilson.workshopmongo.repository.UserRepository;
+import com.denilson.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
